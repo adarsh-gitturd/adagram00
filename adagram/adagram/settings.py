2740dad8b1940ff86aae7ff7ebb5a25c6f7457e4
Adarsh Wilson <adarshwilsona@gmail.com>
2023-12-21 04:50:17 +0530

    more progress

--- adagram/adagram/settings.py
+++ adagram/adagram/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/core",
+]
